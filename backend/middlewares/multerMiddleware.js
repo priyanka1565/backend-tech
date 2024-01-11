cbb596e43d302a9a8832cb0e9f2a14746cda7c3a
priyanka1565 <104352972+priyanka1565@users.noreply.github.com>
2024-01-11 09:53:30 +0530

    added

--- backend/middlewares/multerMiddleware.js
+++ backend/middlewares/multerMiddleware.js
@@ -1,7 +1,7 @@
 const multer = require("multer")
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, ".public/temp/priyanka-image-fotor-bg-remover-20231129112514.png")
+      cb(null, "./public/temp")
     },
     filename: function (req, file, cb) {
       
